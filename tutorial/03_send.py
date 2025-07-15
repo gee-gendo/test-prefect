57613a276eaafeb7b3c3c919e2603c4f5cfc4063
gee-gendo <gee.castro@gendo.ai>
2025-07-15 20:44:25 +0100

    print, AND reply

--- tutorial/03_send.py
+++ tutorial/03_send.py
@@ -34,7 +34,7 @@ async def greeter():
             print("Goodbye!")
             return
         print(f"Received name: {name_input.value}")
-        # await name_input.respond(f"Hello, {name_input.value}!")
+        await name_input.respond(f"Hello, {name_input.value}!")
 
         seen_keys_block.value.append(name_input.metadata.key)
         await seen_keys_block.save(
