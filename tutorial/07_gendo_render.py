04600749c9b8013db2f4f07939669915b1d708ee
gee-gendo <gee.castro@gendo.ai>
2025-08-06 09:43:28 +0100

    refactor to renderJob

--- tutorial/07_gendo_render.py
+++ tutorial/07_gendo_render.py
@@ -9,85 +9,12 @@ from email.message import Message
 
 from prefect import flow, task, get_run_logger
 from prefect.blocks.system import Secret
+from pydantic import BaseModel
+
+from runpod_client import run_runpod_job
 
 # staging
 RENDER_ENDPOINT_ID = "oh1xx5fpoeogpw"
-
-
-class Response(typing.NamedTuple):
-    body: str
-    headers: Message
-    status: int
-    error_count: int = 0
-
-    def json(self) -> typing.Any:
-        """
-        Decode body's JSON.
-
-        Returns:
-            Pythonic representation of the JSON object
-        """
-        try:
-            output = json.loads(self.body)
-        except json.JSONDecodeError:
-            output = ""
-        return output
-
-
-def request(
-    url: str,
-    data: typing.Optional[dict] = None,
-    params: typing.Optional[dict] = None,
-    headers: typing.Optional[dict] = None,
-    method: str = "GET",
-    data_as_json: bool = True,
-    error_count: int = 0,
-) -> Response:
-    if not url.casefold().startswith("http"):
-        raise urllib.error.URLError("Incorrect and possibly insecure protocol in url")
-    method = method.upper()
-    request_data = None
-    headers = headers or {}
-    data = data or {}
-    params = params or {}
-    headers = {"Accept": "application/json", **headers}
-
-    if method == "GET":
-        params = {**params, **data}
-        data = None
-
-    if params:
-        url += "?" + urllib.parse.urlencode(params, doseq=True, safe="/")
-
-    if data:
-        if data_as_json:
-            request_data = json.dumps(data).encode()
-            headers["Content-Type"] = "application/json; charset=UTF-8"
-        else:
-            request_data = urllib.parse.urlencode(data).encode()
-
-    httprequest = urllib.request.Request(
-        url, data=request_data, headers=headers, method=method
-    )
-
-    try:
-        with urllib.request.urlopen(httprequest) as httpresponse:
-            response = Response(
-                headers=httpresponse.headers,
-                status=httpresponse.status,
-                body=httpresponse.read().decode(
-                    httpresponse.headers.get_content_charset("utf-8")
-                ),
-            )
-    except urllib.error.HTTPError as e:
-        response = Response(
-            body=str(e.reason),
-            headers=e.headers,
-            status=e.code,
-            error_count=error_count + 1,
-        )
-
-    return response
 
 
 def get_runpod_api_key() -> str:
@@ -95,8 +22,25 @@ def get_runpod_api_key() -> str:
     if api_key:
         return api_key
     else:
-        secret = Secret.load("runpod-api-key-dev")
+        secret: Secret[str] = Secret.load("runpod-api-key-dev")  # type: ignore
         return secret.get()
+
+
+class RenderJob(BaseModel):
+    prompt: str
+    input_image: str
+    upload_url: str
+    # controlnet parameters
+    control_image_strength: float = 1.0
+    canny_strength: float = 0.0
+    depth_strength: float = 0.0
+    # IP adapter parameters
+    ref_image: str | None = None
+    image_prompt_strength: float = 1.0
+    # System parameters
+    system_negative_prompt: str = "ugly, pixel art"
+    safety_checker_threshold: float = 0.5
+    seed: int = 28337543
 
 
 @task
@@ -107,66 +51,27 @@ def post_runpod_job(job: dict) -> dict:
     api_key = get_runpod_api_key()
 
     # Construct the RunPod API URL
-    url = f"https://api.runpod.ai/v2/{RENDER_ENDPOINT_ID}/run"
-
-    # Set up headers
-    headers = {"authorization": api_key, "content-type": "application/json"}
-
-    # Make the request using the request function
-    response = request(
-        url=url, data=job, headers=headers, method="POST", data_as_json=True
-    )
-
-    # Log the response
-    logger.info(f"RunPod API response status: {response.status}")
-    logger.info(f"RunPod API response: {response.body}")
-
-    # Check for errors
-    if response.status != 200:
-        raise Exception(f"RunPod API error: {response.status} - {response.body}")
-    request_id = response.json().get("id")
-    if not request_id:
-        raise Exception(f"Request ID not found in response: {response.body}")
-    status_url = f"https://api.runpod.ai/v2/{RENDER_ENDPOINT_ID}/status/{request_id}"
-    while True:
-        response = request(
-            url=status_url,
-            headers=headers,
-            method="GET",
-        )
-        logger.info(f"RunPod API response ({response.status}): {response.body}")
-        if response.status == 200:
-            if response.json().get("status").upper() in [
-                "COMPLETED",
-                "FAILED",
-                "TIMED_OUT",
-                "CANCELLED",
-            ]:
-                break
-        time.sleep(1)
-    # Parse and return the JSON response
-    return response.json()
+    r = run_runpod_job(job, logger, api_key, RENDER_ENDPOINT_ID)
+    return r
 
 
 @flow
-def render(job: dict = {}) -> dict:
-    input_image = "https://gendo-gee-dev.s3.amazonaws.com/test/base_interior.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAQ2GNW6JS4PXMQRNG%2F20250716%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250716T124443Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEUaCWV1LXdlc3QtMiJHMEUCIQD5dJPc0x85cDKIpqct2g558lXjJX1sGvF2qCXT7lyDuwIgD%2Fejt5A3hcIW%2FG2p3pmLA9Kq4uWsHfOVZnZXBRPfBn4qoAIIXhABGgwwNTYyNjU5OTQ4NTMiDPJd2SfErnQPS9FNRir9AYzFlr0wz4Fpc7H%2Btqke2Pxa%2F2Zhcph1KLHqcOQOArlXx12ufmTHGUtVXNg%2Fuz69O%2Fow021FQbupl7TE0WYx95lnTUryD1arJmOZ23Kxyu%2BEcPo7TwRqNOzAje2bb1%2B7yVw5eoocbMbUCxoMnlUCwcYAyUWDG3ePRLU6aaN%2FdiLbI89EhVrl%2FIBX73jPvWS9uHKqWQioNyQBej5%2FVV7oZJUvbm9pplh62ZX5dN2VnnCFNNcjjlbP9jwuwI6xrJSDdk8lICzAPMXcwcIJOYk%2FnNZ%2BFn5dvYrD4XdceugPEEcyy0BQH6LE8fvFbw8KELnmAo%2Fji%2FMECXgAdHUR33MwmL3ewwY6nQHB34LviwtOVff4SqT8B6SlXrOf7RhpfdaX0VcGQj0vLNCoSXleGYwdE67rYahv3kc4QUVPbGg6GISLSpzFeaLkz3lYYgCqcQV77zw8D12eh94Or3QponiAN2NeuPs1STOXgu1jr9PcM0PlZ9vChg6m%2FI5gvXLgaOdxTZLn1wAG1f0xQ%2FXgbKftLq919qweRHBVADcKeZsosNA56808&X-Amz-Signature=390274070e79ae720457ef8dfa84dc37e49967e14f6ecb45638f4676f6ceeaff"
-    ref_image = "https://gendo-gee-dev.s3.amazonaws.com/test/standing-06.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAQ2GNW6JSXZVCBIHN%2F20250716%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250716T133412Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEYaCWV1LXdlc3QtMiJHMEUCIGNYHi70r5E4yZ4%2BOxmyBvR1gLIWh43vAuCPFMh0F8l2AiEAlieIOT0qDMXS3AddWmUfewyiIcUaJTDmd4wrlblHBwsqoAIIXxABGgwwNTYyNjU5OTQ4NTMiDIeDWKdosiMa0cOHgir9AdPc4zo%2BSgswdNAcGK%2FYqkYvCOU68jC4bA%2BSJ8I%2FTo9xZ%2FOPyUo26cVYPaMmozBods0EljE%2FZvZdYnOtxvUDYSSYuMdvht87%2FCVcHk%2Fsf0Z%2F7%2BOzC7Gzsd5w%2BKDUuKdaA9F6I%2Bdk6oNILevetLkTX%2FbsgzA9YYz7s3XybEGqWgkGkFZfteV7bei0UAgdw95Mpnu4cBFNN%2BUjbK0Xdz0XNNBOlH1Gw9GGnDJyTdbap4C0cPT3OFplfzb0Hg1FO1OrRJGn9ar2vpg%2FteoUthaqIy7bcK5zUN9qgYBYhdrk5S902HESwBXPgjiT8JDFmgz8iIWi0MEWOi2lFuZKANYwxtTewwY6nQFuL%2FmRovrK4NL%2BbsOAXguF79WozlPK1j93YfqO5XnMYdlyp3nbGUThiykqdVfN14oISgGQU8T0IRRUc6PQs61fhS7odF2dVMkbme%2F7A%2BAQG7X5E9%2BZOSzJ59AlM4UcumhQQeXtOKXOQfD8vHfFjk0zCV33TUWiCN0Gm0iM1J%2Bpn2JiO1hq3dR27F7VAEltADhZZEg5tPDn8JGb4G7x&X-Amz-Signature=bb41c88584f3a87029ffe4af79ef1bad01d287fab31407b7638cc7039e27ebc3"
-    upload_url = "https://gendo-gee-dev.s3.amazonaws.com/test/prefect-output.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAQ2GNW6JSXZVCBIHN%2F20250716%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250716T133447Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEEYaCWV1LXdlc3QtMiJHMEUCIGNYHi70r5E4yZ4%2BOxmyBvR1gLIWh43vAuCPFMh0F8l2AiEAlieIOT0qDMXS3AddWmUfewyiIcUaJTDmd4wrlblHBwsqoAIIXxABGgwwNTYyNjU5OTQ4NTMiDIeDWKdosiMa0cOHgir9AdPc4zo%2BSgswdNAcGK%2FYqkYvCOU68jC4bA%2BSJ8I%2FTo9xZ%2FOPyUo26cVYPaMmozBods0EljE%2FZvZdYnOtxvUDYSSYuMdvht87%2FCVcHk%2Fsf0Z%2F7%2BOzC7Gzsd5w%2BKDUuKdaA9F6I%2Bdk6oNILevetLkTX%2FbsgzA9YYz7s3XybEGqWgkGkFZfteV7bei0UAgdw95Mpnu4cBFNN%2BUjbK0Xdz0XNNBOlH1Gw9GGnDJyTdbap4C0cPT3OFplfzb0Hg1FO1OrRJGn9ar2vpg%2FteoUthaqIy7bcK5zUN9qgYBYhdrk5S902HESwBXPgjiT8JDFmgz8iIWi0MEWOi2lFuZKANYwxtTewwY6nQFuL%2FmRovrK4NL%2BbsOAXguF79WozlPK1j93YfqO5XnMYdlyp3nbGUThiykqdVfN14oISgGQU8T0IRRUc6PQs61fhS7odF2dVMkbme%2F7A%2BAQG7X5E9%2BZOSzJ59AlM4UcumhQQeXtOKXOQfD8vHfFjk0zCV33TUWiCN0Gm0iM1J%2Bpn2JiO1hq3dR27F7VAEltADhZZEg5tPDn8JGb4G7x&X-Amz-Signature=d33e1bfc99b46b4ae310454dca574b1f6b0a9ec071c6532126cc4462e87cba90"
+def render(job: RenderJob) -> dict:
     render_parmas = {
-        "prompt": "Picasso style painting of a house",
-        "system_negative_prompt": "ugly, pixel art",
-        "input_image_presigned_url": input_image,
-        "upload_url": upload_url,
-        "seed": 28337543,
-        "safety_checker_threshold": 0.5,
-        "control_image_strength": 1.99,
-        "canny_strength": 0.0,
-        "depth_strength": 0.0,
-        "style_reference_image_presigned_url": ref_image,  # Optional
-        "image_prompt_strength": 1.8,  # Optional
+        "prompt": job.prompt,
+        "system_negative_prompt": job.system_negative_prompt,
+        "input_image_presigned_url": job.input_image,
+        "upload_url": job.upload_url,
+        "seed": job.seed,
+        "safety_checker_threshold": job.safety_checker_threshold,
+        "control_image_strength": job.control_image_strength,
+        "canny_strength": job.canny_strength,
+        "depth_strength": job.depth_strength,
+        "style_reference_image_presigned_url": job.ref_image,  # Optional
+        "image_prompt_strength": job.image_prompt_strength,  # Optional
     }
-    job = {
+
+    job_dict = {
         "input": {
             "feature": "render",  # Name of the feature to use
             "parameters": render_parmas,
@@ -178,12 +83,17 @@ def render(job: dict = {}) -> dict:
             "external_webhook_url": None,  # Required for speckle handler
         },
     }
-    if job == {}:
+    if job_dict == {}:
         raise ValueError("Job is empty")
 
-    return post_runpod_job(job)
+    return post_runpod_job(job_dict)
 
 
 if __name__ == "__main__":
-    job = {}
+    job = RenderJob(
+        prompt = "Picasso style painting of a haidresser shop",
+        input_image = "https://gendo-gee-dev.s3.amazonaws.com/test/render-gendo/axo-004.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAQ2GNW6JS6HBCNAF7%2F20250806%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250806T084116Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDkaCWV1LXdlc3QtMiJGMEQCIFgPIMYpdSe%2FLOwNSd1Qd6zMQpv7x8%2BePxSqYdOVKw6oAiA8mo82CQ1qFAkf0WhVmf4KDC0g2n1pWxjdk6QVgOn%2FICqgAghyEAEaDDA1NjI2NTk5NDg1MyIMdrcNX86CRsDgD7YhKv0ByonTqMlomr21BRSs2d9cqDkMHKoZlstCn5ZrMpqSwgTztHrKtVz0K2JohzJYLfv6B1exQwAhNIlnRYS5t2cKXheSQw%2BdD2C%2BjH3UGNFrBPwvcHSRDvXMk2UJlYWaK7iNsZG2JtfWfiXjCUDKX3%2FhDabv1Cf4aB7sGzUi0RnDU8hmQyk1WWe78bjLCDibRNp8ag9czmBfLVN5HEsrY87Hd2vIiuJ4jaCs8C2xZQSzOJoY0xTK%2FSwWbHOi2mvehSFn%2BYRXpcqT9WqifOQmr%2Fq5yGIs6CEBban8mW%2BJASN1AK4Eslmll6NHofjODMfP1vYruW1JpwSOhDW%2FQB4e%2FjDhqMzEBjqeAQfWf6%2F0wx11LfzvbdsByElQc%2FQxd%2BqkozeCNwbzoiFEzv0VDBBwCA8Vk28SkpESXPs2L3sRHeoufARqjfBVw5JhI9a%2FeAE%2BQnO6lUIasgyki7lF%2BmJOEqqqzgA9bJbhBwrQ5fGUh3f%2BqBl4%2BWguTZnFUrL%2FlfcycbwsDb%2FD5BAdUADKP9MtUCr8lzPr3UvQ%2F4Veu25%2F9d92OaVlUcF7&X-Amz-Signature=70e0da762d822003210c88e4f4089d3b58548cef001be83edd5ebfde71b40a50",
+        upload_url= "https://gendo-gee-dev.s3.amazonaws.com/test/result.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAQ2GNW6JS6HBCNAF7%2F20250806%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250806T084016Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEDkaCWV1LXdlc3QtMiJGMEQCIFgPIMYpdSe%2FLOwNSd1Qd6zMQpv7x8%2BePxSqYdOVKw6oAiA8mo82CQ1qFAkf0WhVmf4KDC0g2n1pWxjdk6QVgOn%2FICqgAghyEAEaDDA1NjI2NTk5NDg1MyIMdrcNX86CRsDgD7YhKv0ByonTqMlomr21BRSs2d9cqDkMHKoZlstCn5ZrMpqSwgTztHrKtVz0K2JohzJYLfv6B1exQwAhNIlnRYS5t2cKXheSQw%2BdD2C%2BjH3UGNFrBPwvcHSRDvXMk2UJlYWaK7iNsZG2JtfWfiXjCUDKX3%2FhDabv1Cf4aB7sGzUi0RnDU8hmQyk1WWe78bjLCDibRNp8ag9czmBfLVN5HEsrY87Hd2vIiuJ4jaCs8C2xZQSzOJoY0xTK%2FSwWbHOi2mvehSFn%2BYRXpcqT9WqifOQmr%2Fq5yGIs6CEBban8mW%2BJASN1AK4Eslmll6NHofjODMfP1vYruW1JpwSOhDW%2FQB4e%2FjDhqMzEBjqeAQfWf6%2F0wx11LfzvbdsByElQc%2FQxd%2BqkozeCNwbzoiFEzv0VDBBwCA8Vk28SkpESXPs2L3sRHeoufARqjfBVw5JhI9a%2FeAE%2BQnO6lUIasgyki7lF%2BmJOEqqqzgA9bJbhBwrQ5fGUh3f%2BqBl4%2BWguTZnFUrL%2FlfcycbwsDb%2FD5BAdUADKP9MtUCr8lzPr3UvQ%2F4Veu25%2F9d92OaVlUcF7&X-Amz-Signature=a86be6535bfd3f2ca6ca6b18be0049481754525fa5235d699bdb5496500ff631",
+    )
     render.fn(job)
+    print(job)
---
+++ tutorial/runpod_client.py
@@ -0,0 +1,133 @@
+import json
+import typing
+import urllib.error
+import urllib.parse
+import urllib.request
+import time
+import logging
+
+from email.message import Message
+
+
+class Response(typing.NamedTuple):
+    body: str
+    headers: Message
+    status: int
+    error_count: int = 0
+
+    def json(self) -> typing.Any:
+        """
+        Decode body's JSON.
+
+        Returns:
+            Pythonic representation of the JSON object
+        """
+        try:
+            output = json.loads(self.body)
+        except json.JSONDecodeError:
+            output = ""
+        return output
+
+
+def request(
+    url: str,
+    data: typing.Optional[dict] = None,
+    params: typing.Optional[dict] = None,
+    headers: typing.Optional[dict] = None,
+    method: str = "GET",
+    data_as_json: bool = True,
+    error_count: int = 0,
+) -> Response:
+    if not url.casefold().startswith("http"):
+        raise urllib.error.URLError("Incorrect and possibly insecure protocol in url")
+    method = method.upper()
+    request_data = None
+    headers = headers or {}
+    data = data or {}
+    params = params or {}
+    headers = {"Accept": "application/json", **headers}
+
+    if method == "GET":
+        params = {**params, **data}
+        data = None
+
+    if params:
+        url += "?" + urllib.parse.urlencode(params, doseq=True, safe="/")
+
+    if data:
+        if data_as_json:
+            request_data = json.dumps(data).encode()
+            headers["Content-Type"] = "application/json; charset=UTF-8"
+        else:
+            request_data = urllib.parse.urlencode(data).encode()
+
+    httprequest = urllib.request.Request(
+        url, data=request_data, headers=headers, method=method
+    )
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = Response(
+                headers=httpresponse.headers,
+                status=httpresponse.status,
+                body=httpresponse.read().decode(
+                    httpresponse.headers.get_content_charset("utf-8")
+                ),
+            )
+    except urllib.error.HTTPError as e:
+        response = Response(
+            body=str(e.reason),
+            headers=e.headers,
+            status=e.code,
+            error_count=error_count + 1,
+        )
+
+    return response
+
+
+def run_runpod_job(
+    job: dict,
+    logger: logging.Logger | logging.LoggerAdapter[logging.Logger],
+    api_key: str,
+    render_endpoint_id: str,
+) -> dict:
+    url = f"https://api.runpod.ai/v2/{render_endpoint_id}/run"
+
+    # Set up headers
+    headers = {"authorization": api_key, "content-type": "application/json"}
+
+    # Make the request using the request function
+    response = request(
+        url=url, data=job, headers=headers, method="POST", data_as_json=True
+    )
+
+    # Log the response
+    logger.info(f"RunPod API response status: {response.status}")
+    logger.info(f"RunPod API response: {response.body}")
+
+    # Check for errors
+    if response.status != 200:
+        raise Exception(f"RunPod API error: {response.status} - {response.body}")
+    request_id = response.json().get("id")
+    if not request_id:
+        raise Exception(f"Request ID not found in response: {response.body}")
+    status_url = f"https://api.runpod.ai/v2/{render_endpoint_id}/status/{request_id}"
+    while True:
+        response = request(
+            url=status_url,
+            headers=headers,
+            method="GET",
+        )
+        logger.info(f"RunPod API response ({response.status}): {response.body}")
+        if response.status == 200:
+            if response.json().get("status").upper() in [
+                "COMPLETED",
+                "FAILED",
+                "TIMED_OUT",
+                "CANCELLED",
+            ]:
+                break
+        time.sleep(1)
+    # Parse and return the JSON response
+    r = response.json()
+    return r
